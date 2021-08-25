e94e549e08d4129345822e10b2f610df32485299
EddieMeli <edenilson.paschoa@mercadolivre.com>
2021-08-25 17:16:22 -0300

    Initial commit

---
+++ src/com/company/Veiculo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Veiculo {
+
+    int vVelocidade = 0,
+        vAceleracao = 0,
+        vPatente = 0;
+    
+    public Veiculo() {
+
+    }
+}
